9b1e4be91db1c262eaf4a03b51b047791fb29e00
BigBusiness117 <67163482+DylanBruhm@users.noreply.github.com>
2021-07-06 17:09:15 -0700

    Waypoints
    made waypoints for the enemy to travel

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+
+    private Waypoints Wpoints;
+    private int waypointIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Wpoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Wpoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, Wpoints.waypoints[waypointIndex].position ) < 0.1f)
+        {
+            waypointIndex++;
+        }
+    }
+}
